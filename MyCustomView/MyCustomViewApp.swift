98a3df87e49b3c851b88011161768f67f3f7a3b9
Chelsea <chelsea@tripwiretech.com>
2023-08-02 13:18:16 +1200

    Initial Commit

---
+++ MyCustomView/MyCustomViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomViewApp.swift
+//  MyCustomView
+//
+//  Created by Charles Suddens on 2/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCustomViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
